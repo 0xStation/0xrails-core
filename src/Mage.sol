71b969e1ee8e79f12517acde556541a5debc6f1a
conner ֎ <38736612+ilikesymmetry@users.noreply.github.com>
2023-08-03 16:09:11 -0700

    Add UUPSUpgradeable to Mage (#2)

--- src/Mage.sol
+++ src/Mage.sol
@@ -6,13 +6,14 @@ import {Guards} from "./guard/Guards.sol";
 import {Extensions} from "./extension/Extensions.sol";
 import {Execute} from "./lib/Execute.sol";
 import {Multicall} from "openzeppelin-contracts/utils/Multicall.sol";
+import {UUPSUpgradeable} from "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
 
 /**
  * A Solidity framework for creating complex and evolving onchain structures.
  * Mage is an acronym for the architecture pattern's four layers: Module, Access, Guard, and Extension.
  * All Mage-inherited contracts receive a batteries-included contract development kit.
  */
-contract Mage is Access, Guards, Extensions, Execute, Multicall {
+contract Mage is Access, Guards, Extensions, Execute, Multicall, UUPSUpgradeable {
     function contractURI() public view virtual returns (string memory uri) {}
 
     function supportsInterface(bytes4 interfaceId)
@@ -39,6 +40,10 @@ contract Mage is Access, Guards, Extensions, Execute, Multicall {
     }
 
     function _checkCanExecute() internal view override {
+        _checkSenderIsAdmin();
+    }
+
+    function _authorizeUpgrade(address) internal view override {
         _checkSenderIsAdmin();
     }
 }
