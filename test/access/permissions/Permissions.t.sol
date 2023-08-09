389a2b09ed2c9696f7cbde2e55291eec3768e2b5
robriks <marsterlund@gmail.com>
2023-08-09 17:24:44 -0400

    WIP Permissions tests: renouncePermission()

--- test/access/permissions/Permissions.t.sol
+++ test/access/permissions/Permissions.t.sol
@@ -13,6 +13,10 @@ contract PermissionsTest is Test, Permissions {
 
     // to store expected revert errors
     bytes err;
+
+    // errors from IPermissions.sol
+    // error PermissionDoesNotExist(bytes8 operation, address account);
+    // error PermissionAlreadyExists(bytes8 operation, address account);
     
     function setUp() public {
         adminOp = hashOperation('ADMIN');
@@ -48,6 +52,144 @@ contract PermissionsTest is Test, Permissions {
         assertEq(unpackedOp, someOp);
         assertEq(unpackedAddr, someAddress);
     }
+
+    function test_grantPermission(bytes8 operation, bytes8 operation2, address acc, address acc2) public {
+        vm.assume(operation != operation2);
+
+        // ensure permissions do not yet exist
+        bool exists = hasPermission(operation, acc);
+        bool exists2 = hasPermission(operation2, acc2);
+        assertFalse(exists); 
+        assertFalse(exists2);
+        assertEq(getAllPermissions().length, 0);
+
+        // add Permission
+        grantPermission(operation, acc);
+        Permission[] memory firstPermission = getAllPermissions();
+        assertEq(firstPermission.length, 1);
+        assertEq(firstPermission[0].operation, operation);
+        assertEq(firstPermission[0].account, acc);
+        assertEq(firstPermission[0].updatedAt, block.timestamp);
+        assertTrue(hasPermission(operation, acc));
+        _checkPermission(operation, acc); // reverts on failure
+
+        // check storage
+        PermissionsStorage.Layout storage layout = PermissionsStorage.layout();
+        uint256 permissionKey = PermissionsStorage._packKey(operation, acc);
+        PermissionsStorage.PermissionData memory permissionData = layout._permissions[permissionKey];
+        assertTrue(permissionData.exists);
+        assertEq(permissionData.updatedAt, block.timestamp);
+        assertEq(permissionData.index, 0);
+
+        // add Permission2
+        grantPermission(operation2, acc2);
+        Permission[] memory twoPermissions = getAllPermissions();
+        assertEq(twoPermissions.length, 2);
+        assertEq(twoPermissions[1].operation, operation2);
+        assertEq(twoPermissions[1].account, acc2);
+        assertEq(twoPermissions[1].updatedAt, block.timestamp);
+        assertTrue(hasPermission(operation2, acc2));
+        _checkPermission(operation2, acc2); // reverts on failure
+
+        // check storage
+        uint256 permissionKey2 = PermissionsStorage._packKey(operation2, acc2);
+        PermissionsStorage.PermissionData memory permissionData2 = layout._permissions[permissionKey2];
+        assertTrue(permissionData2.exists);
+        assertEq(permissionData2.updatedAt, block.timestamp);
+        assertEq(permissionData2.index, 1);
+    }
+
+    function test_grantPermissionRevertPermissionAlreadyExists(
+        bytes8 operation, 
+        bytes8 operation2, 
+        address acc, 
+        address acc2
+    ) public {
+        vm.assume(acc != acc2); // either operations or accounts may collide but not both
+
+        grantPermission(operation, acc);
+        grantPermission(operation2, acc2);
+
+        err = abi.encodeWithSelector(PermissionAlreadyExists.selector, operation, acc);
+        vm.expectRevert(err);
+        grantPermission(operation, acc);
+        err = abi.encodeWithSelector(PermissionAlreadyExists.selector, operation2, acc2);
+        vm.expectRevert(err);
+        grantPermission(operation2, acc2);
+    }
+
+    function test_revokePermission(address acc, uint8 numPermissions) public {
+        vm.assume(numPermissions > 3);
+        
+        // add permissions
+        for (uint8 i; i < numPermissions; ++i) {
+            // add adminOp Permission to account 
+            grantPermission(bytes8(uint64(adminOp) + i), acc);
+        }
+
+        Permission[] memory permissions = getAllPermissions();
+        assertEq(permissions.length, numPermissions);
+
+        // remove a Permission
+        revokePermission(bytes8(uint64(adminOp) + numPermissions / 2), acc);
+        uint256 newPermissionsLength = getAllPermissions().length;
+        assertEq(newPermissionsLength, permissions.length - 1);
+
+        // remove another Permission
+        revokePermission(adminOp, acc);
+        assertEq(getAllPermissions().length, --newPermissionsLength);
+
+        // revoke the rest
+        for (uint8 j = uint8(newPermissionsLength); j > 0; --j) {
+            // check iter is not the first op to be revoked; j > 0 prevents re-revoking adminOp
+            if (uint64(adminOp) + j == uint64(adminOp) + numPermissions / 2) continue;
+
+            revokePermission(bytes8(uint64(adminOp) + j), acc);
+        }
+    }
+
+    function test_revokePermissionRevertPermissionDoesNotExist(
+        bytes8 operation, 
+        bytes8 operation2,
+        address acc,
+        address acc2 
+    ) public {
+        vm.assume(acc != acc2); // either operations or accounts may collide but not both
+
+        // add permissions
+        grantPermission(operation, acc);
+        grantPermission(operation2, acc2);
+
+        // revoke them, twice
+        revokePermission(operation, acc);
+        err = abi.encodeWithSelector(PermissionDoesNotExist.selector, operation, acc);
+        vm.expectRevert(err);
+        revokePermission(operation, acc);
+
+        revokePermission(operation2, acc2);
+        err = abi.encodeWithSelector(PermissionDoesNotExist.selector, operation2, acc2);
+        vm.expectRevert(err);
+        revokePermission(operation2, acc2);
+    }
+
+    // function test_renouncePermission(address acc, uint8 numPermissions) public {
+    //     vm.assume(numPermissions > 0);
+    //     vm.assume(acc != address(0x0));
+
+    //     // add permissions for acc
+    //     for (uint256 i; i < numPermissions; ++i) {
+    //         grantPermission(bytes8(uint64(adminOp) + uint64(i)), acc);
+    //     }
+
+    //     assertEq(getAllPermissions().length, numPermissions);
+
+    //     // acc doesn't want them, renounces
+    //     vm.startPrank(acc);
+    //     renouncePermission(adminOp);
+    //     assertEq(getAllPermissions().length, numPermissions - 1);
+    // }
+
+    // function test_renouncePermissionRevertPermissionDoesNotExist() public {}
 
     /*==============
         OVERRIDES
