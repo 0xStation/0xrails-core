0c01266816f7dba5f9efe4ac0301cb7745006d39
robriks <marsterlund@gmail.com>
2023-08-17 11:09:19 -0400

    :white_check_mark: Approve and setApprovalForAll tests

--- test/cores/ERC721/ERC721.t.sol
+++ test/cores/ERC721/ERC721.t.sol
@@ -35,17 +35,55 @@ contract ERC721Test is Test {
         for (uint tokenId; tokenId < quantity; tokenId++) {
             assertEq(erc721.ownerOf(tokenId), to);
         }
+        assertEq(erc721.totalMinted(), quantity);
     }
 
     function test_mintRevertZeroQuantity(address to) public {
         vm.expectRevert(IERC721Internal.MintZeroQuantity.selector);
         erc721.mint(to, 0);
+        assertEq(erc721.totalMinted(), 0);
     }
 
     function test_mintRevertZeroAddress(uint8 quantity) public {
         vm.assume(quantity != 0);
         vm.expectRevert(IERC721Internal.MintToZeroAddress.selector);
         erc721.mint(address(0x0), quantity);
+        assertEq(erc721.totalMinted(), 0);
+    }
+
+    function test_safeMint(uint8 quantity) public {
+        vm.assume(quantity > 0);
+        
+        address to = address(erc721Receiver);
+        erc721.safeMint(to, quantity);
+
+        assertEq(erc721.totalSupply(), quantity);
+        assertEq(erc721.balanceOf(to), quantity);
+        for (uint tokenId; tokenId < quantity; tokenId++) {
+            assertEq(erc721.ownerOf(tokenId), to);
+        }
+        assertEq(erc721.totalMinted(), quantity);
+    }
+
+    function test_safeMintRevertTransferToNonERC721Receiver(uint8 quantity) public {
+        vm.assume(quantity > 0);
+        
+        address to = address(this); // test contract doesn't implement onERC721Received()
+        err = abi.encodeWithSelector(IERC721Internal.TransferToNonERC721ReceiverImplementer.selector);
+        vm.expectRevert(err);
+        erc721.safeMint(to, quantity);
+
+        assertEq(erc721.totalSupply(), 0);
+        assertEq(erc721.balanceOf(to), 0);
+        assertEq(erc721.totalMinted(), 0);
+
+        address to2 = address(new ERC721Harness()); // erc721 contract doesn't implement onERC721Received()
+        vm.expectRevert(err);
+        erc721.safeMint(to2, quantity);
+
+        assertEq(erc721.totalSupply(), 0);
+        assertEq(erc721.balanceOf(to2), 0);
+        assertEq(erc721.totalMinted(), 0);
     }
 
     function test_burn(address to, uint8 mintQuantity, uint8 burnQuantity) public {
@@ -63,6 +101,7 @@ contract ERC721Test is Test {
             vm.expectRevert(IERC721Internal.OwnerQueryForNonexistentToken.selector);
             erc721.ownerOf(tokenId);
         }
+        assertEq(erc721.totalBurned(), burnQuantity);
     }
 
     function test_transfer(address from, address to, uint8 mintQuantity, uint8 transferQuantity) public {
@@ -138,10 +177,11 @@ contract ERC721Test is Test {
 
         uint256 preTransferBalanceFrom = erc721.balanceOf(from);
         uint256 preTransferBalanceTo = erc721.balanceOf(to);
+        err = abi.encodeWithSelector(IERC721Internal.TransferToNonERC721ReceiverImplementer.selector);
         // attempt safeTransfers to this address
         for (uint i; i < transferQuantity; i++) {
             uint256 tokenId = i;
-            vm.expectRevert(IERC721Internal.TransferToNonERC721ReceiverImplementer.selector);
+            vm.expectRevert(err);
             erc721.safeTransfer(from, to, tokenId, '');
             // assert transfers not completed
             assertEq(erc721.balanceOf(from), preTransferBalanceFrom);
@@ -154,7 +194,7 @@ contract ERC721Test is Test {
         uint256 preTransferBalanceTo2 = erc721.balanceOf(to2);
         for (uint i; i < transferQuantity; i++) {
             uint256 tokenId = i;
-            vm.expectRevert(IERC721Internal.TransferToNonERC721ReceiverImplementer.selector);
+            vm.expectRevert(err);
             erc721.safeTransfer(from, to2, tokenId, '');
             // assert transfers not completed
             assertEq(erc721.balanceOf(from), preTransferBalanceFrom);
@@ -162,6 +202,119 @@ contract ERC721Test is Test {
             assertEq(erc721.ownerOf(tokenId), from);
         }
         assertEq(erc721.totalSupply(), preTransferBalanceFrom);        
+    }
+
+    function test_approve(
+        address from, 
+        address operator, 
+        uint8 mintQuantity
+    ) public {
+        vm.assume(from != address(0x0) && operator != address(0x0)); // prevent mint/approve to address(0x0)
+        vm.assume(mintQuantity > 0);
+
+        erc721.mint(from, mintQuantity);
+
+        for (uint256 i; i < mintQuantity; ++i) {
+            // no approvals yet
+            assertEq(erc721.getApproved(i), address(0x0));
+            assertFalse(erc721.isApprovedForAll(from, operator));
+
+            // make approval
+            vm.prank(from);
+            erc721.approve(operator, i);
+
+            // assert successful approval
+            assertEq(erc721.getApproved(i), operator);
+            assertFalse(erc721.isApprovedForAll(from, operator));
+        }
+    }
+    function test_approveRevertApprovalInvalidOperator(address from, uint8 mintQuantity) public {
+        vm.assume(from != address(0x0)); // prevent mint/approve to address(0x0)
+        vm.assume(mintQuantity > 0);
+
+        address operator = address(0x0); // reverting operator
+        erc721.mint(from, mintQuantity);
+
+        for (uint256 i; i < mintQuantity; ++i) {
+            // no approvals yet
+            assertEq(erc721.getApproved(i), address(0x0));
+            assertFalse(erc721.isApprovedForAll(from, operator));
+
+            // make reverting approval
+            err = abi.encodeWithSelector(IERC721Internal.ApprovalInvalidOperator.selector);
+            vm.expectRevert(err);
+            vm.prank(from);
+            erc721.approve(operator, i);
+
+            assertEq(erc721.getApproved(i), address(0x0));
+            assertFalse(erc721.isApprovedForAll(from, operator));
+        }
+    }
+
+    function test_approveRevertApprovalCallerNotOwnerNorApproved(
+        address from, 
+        address someAddress, 
+        address operator,
+        uint8 mintQuantity
+    ) public {
+        vm.assume(from != address(0x0) && from != someAddress); // prevent address(0x0) & self-approve
+        vm.assume(mintQuantity > 0);
+
+        erc721.mint(from, mintQuantity);
+
+        for (uint256 i; i < mintQuantity; ++i) {
+            // no approvals yet
+            assertEq(erc721.getApproved(i), address(0x0));
+            assertFalse(erc721.isApprovedForAll(from, someAddress));
+
+            // make reverting approval as someAddress
+            err = abi.encodeWithSelector(IERC721Internal.ApprovalCallerNotOwnerNorApproved.selector);
+            vm.expectRevert(err);
+            vm.prank(someAddress);
+            erc721.approve(operator, i);
+
+            // assert no approvals made
+            assertEq(erc721.getApproved(i), address(0x0));
+            assertFalse(erc721.isApprovedForAll(from, operator));
+        }
+    }
+
+    function test_setApprovalForAll(address from, address operator, uint8 mintQuantity) public {
+        vm.assume(from != address(0x0) && operator != address(0x0)); // prevent address(0x0) approvals
+        vm.assume(mintQuantity > 0);
+  
+        erc721.mint(from, mintQuantity);
+
+        // no approval yet
+        assertFalse(erc721.isApprovedForAll(from, operator));
+
+        vm.prank(from);
+        erc721.setApprovalForAll(operator, true);
+
+        // assert approvalForAll set to true
+        assertTrue(erc721.isApprovedForAll(from, operator));
+    }
+
+    function test_setApprovalForAllRevertApprovalInvalidOperator(
+        address from, 
+        uint8 mintQuantity
+    ) public {
+        vm.assume(from != address(0x0)); // prevent address(0x0) mint
+        vm.assume(mintQuantity > 0);
+  
+        address badOperator = address(0x0);
+        erc721.mint(from, mintQuantity);
+
+        // no approval yet
+        assertFalse(erc721.isApprovedForAll(from, badOperator));
+
+        err = abi.encodeWithSelector(IERC721Internal.ApprovalInvalidOperator.selector);
+        vm.expectRevert(err);
+        vm.prank(from);
+        erc721.setApprovalForAll(badOperator, true);
+
+        // assert setApprovalForAll failed
+        assertFalse(erc721.isApprovedForAll(from, badOperator));
     }
 }
 
