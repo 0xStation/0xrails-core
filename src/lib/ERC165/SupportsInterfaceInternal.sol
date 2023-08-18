4ee260e782a60b2f153a37432d03ee015f27e156
robriks <marsterlund@gmail.com>
2023-08-18 11:37:36 -0400

    :bug: Add explicit ERC165 compliance for SupportsInterface inheritors using the type(IERC165).interfaceId as storage constant

--- src/lib/ERC165/SupportsInterfaceInternal.sol
+++ src/lib/ERC165/SupportsInterfaceInternal.sol
@@ -5,12 +5,18 @@ import {ISupportsInterfaceInternal} from "./ISupportsInterface.sol";
 import {SupportsInterfaceStorage} from "./SupportsInterfaceStorage.sol";
 
 abstract contract SupportsInterfaceInternal is ISupportsInterfaceInternal {
+
+    /// @dev For explicit EIP165 compliance, the interfaceId of the standard IERC165 implementation
+    /// which is derived from `bytes4(keccak256('supportsInterface(bytes4)'))` 
+    /// is stored directly as a constant in order to preserve Mage's ERC7201 namespace pattern
+    bytes4 public constant erc165Id = 0x01ffc9a7;
+
     /*===========
         VIEWS
     ===========*/
     function _supportsInterface(bytes4 interfaceId) internal view returns (bool) {
         SupportsInterfaceStorage.Layout storage layout = SupportsInterfaceStorage.layout();
-        return layout._supportsInterface[interfaceId];
+        return interfaceId == erc165Id || layout._supportsInterface[interfaceId];
     }
 
     /*=============
