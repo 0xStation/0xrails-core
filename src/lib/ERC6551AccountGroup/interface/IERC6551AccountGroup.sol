a22facab545469a619b988d150a9ace3fe8ae813
Conner Swenberg <cls364@cornell.edu>
2023-10-13 15:30:54 -0400

    Remove AccountGroups

--- src/lib/ERC6551AccountGroup/interface/IERC6551AccountGroup.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IERC6551AccountGroup {
-    function getAccountInitializer(address account) external view returns (address initializer);
-}
--- src/lib/ERC6551AccountGroup/interface/IERC6551AccountInitializer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IERC6551AccountInitializer {
-    function initializeAccount(address accountImpl, bytes memory initData) external payable;
-}
