af3e0678b56cadf88fe4fb294ebe63f55723679f
Conner Swenberg <cls364@cornell.edu>
2023-10-14 19:28:31 -0400

    Update AccountProxy and deploy

--- src/lib/ERC6551AccountGroup/AccountProxy.sol
+++ src/lib/ERC6551AccountGroup/AccountProxy.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Upgrade} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import {Proxy} from "openzeppelin-contracts/proxy/Proxy.sol";
 import {ERC6551AccountLib} from "lib/erc6551/src/lib/ERC6551AccountLib.sol";
+
 import {IERC6551AccountInitializer} from "./interface/IERC6551AccountInitializer.sol";
 import {IERC6551AccountGroup} from "./interface/IERC6551AccountGroup.sol";
 
 /// @notice Global Account Proxy to establish if an ERC6551 Account is using the Account Group pattern.
 /// This contract is meant to be a permissionless singleton.
-contract AccountProxy is ERC1967Proxy, IERC6551AccountInitializer {
-    constructor() ERC1967Proxy(address(0), "") {}
-
+contract AccountProxy is Proxy, ERC1967Upgrade, IERC6551AccountInitializer {
     /// @dev should we enforceme that this function can only be delegatecall'ed?
     function initializeAccount(address, bytes memory) external payable {
         // parse accountGroup from first 20 bytes of 6551 Account salt
@@ -19,5 +19,9 @@ contract AccountProxy is ERC1967Proxy, IERC6551AccountInitializer {
         address initializer = IERC6551AccountGroup(accountGroup).getAccountInitializer(address(this));
         // delegate call initializer with received implementation and initialization data
         _delegate(initializer);
+    }
+
+    function _implementation() internal view virtual override returns (address) {
+        return ERC1967Upgrade._getImplementation();
     }
 }
