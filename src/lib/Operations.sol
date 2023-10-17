3b77546be15c5a4d86c75f7ef97fad6aa4926030
Conner Swenberg <cls364@cornell.edu>
2023-10-17 15:39:57 -0400

    Add INITIALIZE_ACCOUNT_PERMIT operation

--- src/lib/Operations.sol
+++ src/lib/Operations.sol
@@ -21,6 +21,7 @@ library Operations {
     bytes8 constant BURN_PERMIT = 0x6801400fea7cd7c7; // hashOperation("BURN_PERMIT");
     bytes8 constant TRANSFER_PERMIT = 0xa994951607abf93b; // hashOperation("TRANSFER_PERMIT");
     bytes8 constant CALL_PERMIT = 0xc8d1733b0840734c; // hashOperation("CALL_PERMIT");
+    bytes8 constant INITIALIZE_ACCOUNT_PERMIT = 0x449384b01ca84f74; // hashOperation("INITIALIZE_ACCOUNT_PERMIT");
 
     /// @dev Function to provide the signature string corresponding to an 8-byte operation
     /// @param name The signature string for an 8-byte operation. Empty for unrecognized operations.
