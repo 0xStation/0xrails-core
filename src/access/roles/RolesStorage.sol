c893349a344822248a1309f9628e07d04bdd3851
Conner Swenberg <cls364@cornell.edu>
2023-11-10 18:59:21 -0500

    Add Roles spike

---
+++ src/access/roles/RolesStorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+library RolesStorage {
+    bytes32 internal constant SLOT = keccak256(abi.encode(uint256(keccak256("0xrails.Roles")) - 1));
+
+    struct Layout {
+        bytes32[] _grantedRoleKeys;
+        mapping(bytes32 => GrantedRoleData) _grantedRoles;
+    }
+
+    struct GrantedRoleData {
+        uint24 index; //       [0..23]
+        uint40 updatedAt; //   [24..63]
+        bool exists; //        [64-71]
+        bytes20 roleSuffix; // [72..232]
+    }
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+
+    // key = 0x{address}{rolePrefix} where rolePrefix is the first 12 bytes of bytes32 role
+    function _packKey(address account, bytes32 role) internal pure returns (bytes32 key) {
+        return bytes32(uint256(uint160(account)) << 96 | (uint96(bytes12(role))));
+    }
+
+    function _unpackKey(bytes32 key) internal pure returns (address account, bytes12 rolePrefix) {
+        account = address(bytes20(key));
+        rolePrefix = bytes12(uint96(uint256(key)));
+        return (account, rolePrefix);
+    }
+}
