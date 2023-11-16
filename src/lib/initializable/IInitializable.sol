8e36ca2466bfedd97ae470d94c479ad57da5627d
robriks <marsterlund@gmail.com>
2023-11-15 21:47:00 -0500

    :art: Collapse Initializable files

--- src/lib/initializable/IInitializable.sol
+++ src/lib/initializable/IInitializable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface IInitializableInternal {
+interface IInitializable {
     // events
     event Initialized();
 
@@ -13,5 +13,3 @@ interface IInitializableInternal {
     /// @dev View function to return whether a proxy contract has been initialized.
     function initialized() external view returns (bool);
 }
-
-interface IInitializable is IInitializableInternal {}
