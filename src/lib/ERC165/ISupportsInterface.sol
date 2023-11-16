788d2120c0eb2ff72dca6596a12d6ce5c27522dd
robriks <marsterlund@gmail.com>
2023-11-15 21:45:27 -0500

    :art: Collapse SupportsInterface files

--- src/lib/ERC165/ISupportsInterface.sol
+++ src/lib/ERC165/ISupportsInterface.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface ISupportsInterfaceInternal {
+interface ISupportsInterface {
     // events
     event InterfaceAdded(bytes4 indexed interfaceId);
     event InterfaceRemoved(bytes4 indexed interfaceId);
@@ -9,9 +9,7 @@ interface ISupportsInterfaceInternal {
     // errors
     error InterfaceAlreadyAdded(bytes4 interfaceId);
     error InterfaceNotAdded(bytes4 interfaceId);
-}
 
-interface ISupportsInterfaceExternal {
     /// @dev Function to implement ERC-165 compliance 
     /// @param interfaceId The interface identifier to check.
     /// @return _ Boolean indicating whether the contract supports the specified interface.
@@ -25,5 +23,3 @@ interface ISupportsInterfaceExternal {
     /// @param interfaceId The interface identifier to remove support for.
     function removeInterface(bytes4 interfaceId) external;
 }
-
-interface ISupportsInterface is ISupportsInterfaceInternal, ISupportsInterfaceExternal {}
